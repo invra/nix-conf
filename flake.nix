0c1efe6107bccc807c5686cd0d64686fbc0bb7fd
Invra <identificationsucks@gmail.com>
2025-01-31 09:39:43 +1100

    Add something
    Like i added too much, just check the diffs bro.

--- flake.nix
+++ flake.nix
@@ -28,23 +28,26 @@
         config.allowUnfree = true;
       };
       pkgs = unstable;
+      config = (builtins.fromTOML (builtins.readFile ./config.toml));
+      user = config.user;
+      development = config.development;
       spicePkgs = spicetify-nix.legacyPackages.${system};
     in {
-      nixosConfigurations.invra = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.${user.username} = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          ./config/configuration.nix
+          (import ./config/configuration.nix user config.system)
           home-manager.nixosModules.home-manager
           {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
               backupFileExtension = "backup";
-              users.invra =
+              users.${user.username} =
                 (import ./home/home.nix spicePkgs inputs);
               extraSpecialArgs = {
-                inherit pkgs unstable stable;
-                username = "invra";
+                inherit pkgs development user unstable stable;
+                username = user.username;
               };
             };
           }
--- home/config.nu
+++ home/config.nu
@@ -2,6 +2,9 @@ $env.config.show_banner = false;
 $env.config.use_kitty_protocol = true;
 
 alias d = cd
+alias nv = nvim
+alias ncim = nvim
+alias gc = git clone
 
 export def greet [name] {
   $"Hello, ($name)!"
