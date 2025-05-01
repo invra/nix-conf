3d80ca81c6736fd115ae4cb60559848a31792cd6
Invra <identificationsucks@gmail.com>
2025-05-01 19:25:51 +1000

    add: bridging support back after making adjustments to allow for no config.

--- config/configuration.nix
+++ config/configuration.nix
@@ -116,9 +116,21 @@ user: system: desktop:
     graphics.enable = true;
   };
 
-  networking = {
+networking = {
     hostName = system.hostname;
     networkmanager.enable = system.networking.networkmanager;
+    firewall.enable = system.networking.firewallEnabled;
+    useDHCP = system.networking.dhcpEnabled;
+
+    interfaces = builtins.listToAttrs (map (iface: {
+      name = iface.name;
+      value.useDHCP = (iface.dhcpEnabled or iface.type != "BRIDGE");
+    }) system.networking.interfaces or []);
+
+    bridges = builtins.listToAttrs (map (iface: {
+      name = iface.name;
+      value.interfaces = (iface.interfaces or []);
+    }) (builtins.filter (iface: iface.type == "BRIDGE") system.networking.interfaces or []));
   };
 
   i18n.defaultLocale = system.locale;
