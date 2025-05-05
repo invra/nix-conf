396c397b9dc20a425a6478ba4202f8ee9ddba6a7
Invra <identificationsucks@gmail.com>
2025-05-06 02:56:30 +1000

    remove: hyprpanel

--- home/system/hyprpanel.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, ... }: {
-  imports = [ inputs.hyprpanel.homeManagerModules.hyprpanel ];
-
-  programs.hyprpanel = {
-    enable = true;
-    systemd.enable = true;
-    hyprland.enable = true;
-  };
-}
