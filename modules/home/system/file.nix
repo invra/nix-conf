9a150f84f89a4fd3139994c2f517af4367eab4ce
InvraNet <identificationsucks@gmail.com>
2025-05-11 02:40:28 +1000

    Move all of VSCode to a HM-System.

--- modules/home/system/file.nix
+++ modules/home/system/file.nix
@@ -4,8 +4,6 @@
     ".config/starship.toml" = {
       source = ./config/starship.toml;
     };
-    ".config/Code/User/settings.json" = {
-      source = ./config/vscode/settings.json;
-    };
+
   };
 }
