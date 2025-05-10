7253f81411cc1a3e6cc52c8124b55d08664dc2ed
Invra <identificationsucks@gmail.com>
2025-05-10 23:12:21 +1000

    Change: in margins.

--- modules/home/programs/hyprland/default.nix
+++ modules/home/programs/hyprland/default.nix
@@ -63,7 +63,7 @@ in
 
       # General settings
       general = {
-        gaps_in = 5;
+        gaps_in = 2.5;
         gaps_out = 5;
         border_size = 3;
         "col.active_border" = "rgb(ebbcba) rgb(ebbcba) 45deg";
