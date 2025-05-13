45c164365b9a26f2ecd691a640f12e39d37a8ee5
Hiten Tandon <tandonhiten@gamil.com>
2025-05-13 22:34:51 +0530

    WIP

--- modules/home//default.nix
+++ modules/home//default.nix
@@ -1,1 +1,19 @@
-{ ... }: { }
+{
+  nixpkgs,
+  user,
+  ...
+}:
+let
+  utils = import ./utils.nix { inherit (nixpkgs) lib; };
+in
+{
+  imports = utils.getModulesFromDirsRec [
+    ./misc
+    ./programs
+  ];
+
+  home = {
+    username = user.username;
+    homeDirectory = "/home/${user.username}";
+  };
+}
