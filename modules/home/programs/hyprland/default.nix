b4647494c2becf9d6e8a09e5699cbfdc2f61643d
Hiten Tandon <tandonhiten@gamil.com>
2025-05-13 23:23:46 +0530

    Refactor: Rearranged some lists to be better sorted

--- modules/home/programs/hyprland/default.nix
+++ modules/home/programs/hyprland/default.nix
@@ -1,35 +1,24 @@
 { unstable, desktop, ... }:
-let
-  hyprland = desktop.hyprland;
-
-  monitors = builtins.map (monitor: {
-    name = monitor.name;
-    resolution = monitor.resolution;
-    refreshRate = monitor.refreshRate;
-    position = monitor.position;
-    scale = monitor.scale;
-  }) hyprland.monitors;
-in
 {
-  imports = [
-    ./mako/mako.nix
-    ./waybar/waybar.nix
-  ];
+  imports = [ ./mako ./waybar ];
   stylix.targets.hyprland.enable = false;
 
-  home.packages = with unstable; [
-    playerctl
-    hyprshot
-  ];
+  home.packages = with unstable; [ playerctl hyprshot ];
 
   wayland.windowManager.hyprland = {
-    enable = hyprland.enable;
+    inherit (desktop.hyprland) enable;
 
     settings = {
       monitor = builtins.map (
-        monitor:
-        "${monitor.name},${monitor.resolution}@${builtins.toString monitor.refreshRate},${monitor.position},${builtins.toString monitor.scale}"
-      ) monitors;
+        {
+          name,
+          resolution,
+          refreshRate,
+          position,
+          scale,
+        }:
+        "${name},${resolution}@${refreshRate},${position},${scale}"
+      ) (builtins.mapAttrs (_: builtins.toString) desktop.hyprland.monitors);
 
       # Auto-launching
       exec-once = [
@@ -51,9 +40,7 @@ in
         rounding = 10;
         active_opacity = 1;
         inactive_opacity = 0.8;
-        shadow = {
-          enabled = false;
-        };
+        shadow.enabled = false;
         blur = {
           enabled = true;
           size = 5;
@@ -61,18 +48,14 @@ in
         };
       };
 
-      animations = {
-        enabled = true;
-      };
+      animations.enabled = true;
 
       # Inputs (keyboard & mouse)
       input = {
         kb_layout = "us";
         follow_mouse = 1;
         sensitivity = 0.5;
-        touchpad = {
-          natural_scroll = true;
-        };
+        touchpad.natural_scroll = true; 
       };
 
       gestures = {
@@ -80,9 +63,7 @@ in
         workspace_swipe_fingers = 3;
       };
 
-      render = {
-        explicit_sync = false;
-      };
+      render.explicit_sync = false;
 
       dwindle = {
         pseudotile = true;
@@ -91,38 +72,38 @@ in
 
       # Keybinds
       bind = [
-        "SUPER, Return, exec, ghostty"
+        # { mod = with keys; [SUPER B], exec = "zen" }
+        "SUPER, B, exec, zen"
+        "SUPER, Q, killactive"
+        "SUPER, C, togglesplit"
+        "SUPER, 1, workspace, 1"
+        "SUPER, 2, workspace, 2"
+        "SUPER, 3, workspace, 3"
+        "SUPER, 4, workspace, 4"
+        "SUPER, 5, workspace, 5"
         "SUPER, S, exec, spotify"
         "SUPER, Z, exec, zeditor"
         "SUPER, D, exec, vesktop"
-        "SUPER, T, exec, thunar"
-        "SUPER, V, exec, clipman pick -t rofi"
-        "SUPER, B, exec, zen"
-        "SUPER, Q, killactive"
-        "SUPER ALT SHIFT, Q, exit"
-        "SUPER LSHIFT, S, exec, hyprshot -m region --clipboard-only"
-        "SUPER LSHIFT, Space, togglefloating"
-        "SUPER, C, togglesplit"
         "ALT, Return, fullscreen"
-        "SUPER, 1, workspace, 1"
+        "SUPER ALT SHIFT, Q, exit"
+        "SUPER, Return, exec, ghostty"
         "SUPER SHIFT, 1, movetoworkspace, 1"
-        "SUPER, 2, workspace, 2"
         "SUPER SHIFT, 2, movetoworkspace, 2"
-        "SUPER, 3, workspace, 3"
         "SUPER SHIFT, 3, movetoworkspace, 3"
-        "SUPER, 4, workspace, 4"
         "SUPER SHIFT, 4, movetoworkspace, 4"
-        "SUPER, 5, workspace, 5"
         "SUPER SHIFT, 5, movetoworkspace, 5"
+        "SUPER LSHIFT, Space, togglefloating"
+        "SUPER, V, exec, clipman pick -t rofi"
+        "SUPER LSHIFT, S, exec, hyprshot -m region --clipboard-only"
       ];
 
       bindl = [
-        " , XF86AudioPrev, exec, playerctl previous"
-        "ALT LSHIFT, F10, exec, playerctl previous"
-        " , XF86AudioPlay, exec, playerctl play-pause"
-        "ALT LSHIFT, F11, exec, playerctl play-pause"
-        " , XF86AudioNext, exec, playerctl next"
         "ALT LSHIFT, F12, exec, playerctl next"
+        " , XF86AudioNext, exec, playerctl next"
+        "ALT LSHIFT, F10, exec, playerctl previous"
+        " , XF86AudioPrev, exec, playerctl previous"
+        "ALT LSHIFT, F11, exec, playerctl play-pause"
+        " , XF86AudioPlay, exec, playerctl play-pause"
       ];
 
       bindr = [
@@ -136,15 +117,14 @@ in
       ];
 
       binde = [
-        " , XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
         " , XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        " , XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
       ];
 
       windowrulev2 = [
         "pin, title:Picture-in-Picture"
         "float, title:Picture-in-Picture"
         "size 640 360, title:Picture-in-Picture"
-        "pin, title:Picture-in-Picture"
       ];
     };
   };
