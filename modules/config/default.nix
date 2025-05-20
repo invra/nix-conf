5988bfaac4ae8455b8d77cbd8c490816058229cd
InvraNet <identificationsucks@gmail.com>
2025-05-20 13:13:38 +1000

    Seperate more.

--- modules/config/default.nix
+++ modules/config/default.nix
@@ -1,21 +1,21 @@
 { unstable, ... }:
 {
-  imports = if unstable.stdenv.isLinux then [ ./linux.nix ] else [./darwin.nix];
+  imports = if unstable.stdenv.isLinux then [ ./linux ] else [ ./darwin ];
 
   nix = {
-  settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    extra-experimental-features = [
-      "pipe-operators"
-    ];
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      extra-experimental-features = [
+        "pipe-operators"
+      ];
 
-    substituters = [ "https://nix-community.cachix.org" ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
+      substituters = [ "https://nix-community.cachix.org" ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 }
