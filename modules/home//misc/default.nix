c4c2e76df8bb27fc4dc90e02a71e28ccf6d65ae6
Invra <identificationsucks@gmail.com>
2025-05-21 15:06:25 +1000

    Formatting: sort.

--- modules/home//misc/default.nix
+++ modules/home//misc/default.nix
@@ -3,12 +3,11 @@
   home = {
     homeDirectory = "/home/${user.username}";
     packages = with unstable; [
-      chromium
-      signal-desktop-bin
-      parsec-bin
       vlc
       wayvnc
+      chromium
+      parsec-bin
+      signal-desktop-bin
     ];
   };
-
 }
