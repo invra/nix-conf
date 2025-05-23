238277db5c1ccab0543f1fea2a2917754149d52a
Invra <identificationsucks@gmail.com>
2025-05-23 11:55:25 +1000

    Add: davinci resolve

--- modules/home//misc/default.nix
+++ modules/home//misc/default.nix
@@ -4,12 +4,14 @@
     homeDirectory = "/home/${user.username}";
     packages = with unstable; [
       vlc
+      gimp3
       wayvnc
       helvum
       chromium
       parsec-bin
       easyeffects
       pavucontrol
+      davinci-resolve
       signal-desktop-bin
     ];
   };
