bb831bb5e682b2598b6ffcb682f9f6d5f3fece3a
InvraNet <identificationsucks@gmail.com>
2025-05-27 01:06:42 +1000

    Fix: module name mixup.

--- modules/config/linux/options/virtualisation.nix
+++ modules/config/linux/options/virtualisation.nix
@@ -1,23 +1,16 @@
-{ system, unstable, ... }:
+{ ... }:
 {
-  networking = {
-    hostName = system.hostname;
-    networkmanager.enable = system.networking.networkmanager;
-    firewall.enable = system.networking.firewallEnabled;
-    useDHCP = unstable.lib.mkForce system.networking.dhcpEnabled;
-
-    interfaces = builtins.listToAttrs (
-      map (iface: {
-        name = iface.name;
-        value.useDHCP = (iface.dhcpEnabled or iface.type != "BRIDGE");
-      }) system.networking.interfaces or [ ]
-    );
-
-    bridges = builtins.listToAttrs (
-      map (iface: {
-        name = iface.name;
-        value.interfaces = (iface.interfaces or [ ]);
-      }) (builtins.filter (iface: iface.type == "BRIDGE") system.networking.interfaces or [ ])
-    );
+  virtualisation = {
+    docker.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu.ovmf.enable = true;
+    };
+    spiceUSBRedirection.enable = true;
+    vmVariant.virtualisation = {
+      memorySize = 1024 * 32;
+      cores = 8;
+      diskSize = 128 * 1024;
+    };
   };
 }
