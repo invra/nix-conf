f01359d4f76d13799d5078039917a76c67eba20b
Invra <identificationsucks@gmail.com>
2025-05-27 00:42:20 +1000

    Add: config for virtual machines.

---
+++ configurations/vm/default.nix
@@ -0,0 +1,77 @@
+{
+  desktop = {
+    hyprland = {
+      enable = true;
+      monitors = [
+        {
+          name = "DP-3";
+          position = "0x0";
+          refreshRate = 180;
+          resolution = "2560x1440";
+          scale = 1;
+        }
+      ];
+    };
+    plasma = {
+      enable = true;
+    };
+  };
+  development = {
+    git = {
+      defaultBranch = "main";
+      email = "identificationsucks@gmail.com";
+      types = [
+        "GitLab"
+        "GitHub"
+      ];
+      username = "InvraNet";
+    };
+  };
+  system = {
+    hardware-module = ./hardware-configuration.nix;
+    dock = {
+      autoHideDelay = 0.45;
+      autohide = true;
+      orientation = "right";
+    };
+    graphics = {
+      blacklists = [
+        "nouveau"
+        "nvidia"
+      ];
+      wanted = [ "amdgpu" ];
+    };
+    greeter = "gdm";
+    hostname = "NixOS";
+    interfaces = { };
+    kernelParams = [ "intel_iommu=on" ];
+    locale = "en_AU.UTF-8";
+    networking = {
+      dhcpEnabled = true;
+      firewallEnabled = false;
+      networkmanager = true;
+      # interfaces = [
+      #   {
+      #     type = "BRIDGE";
+      #     name = "br0";
+      #     interfaces = [ "enp6s0" ];
+      #     dhcpEnabled = true;
+      #   }
+      # ];
+    };
+    services = {
+      mongodb = {
+        enable = true;
+      };
+      ssh = {
+        enable = true;
+      };
+    };
+    timezone = "Australia/Sydney";
+  };
+  user = {
+    displayName = "Invraa (>.<)";
+    initialPassword = "123456";
+    username = "invra";
+  };
+}
