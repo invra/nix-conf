88547910dc4d3cd6a513858322f688f2b1d664bf
Invra <identificationsucks@gmail.com>
2025-05-31 15:01:21 +1000

    Move: mandoc's to Linux only.

--- modules/config/default.nix
+++ modules/config/default.nix
@@ -26,11 +26,6 @@
     pkgs = unstable;
   };
 
-  documentation.man = {
-    man-db.enable = false;
-    mandoc.enable = true;
-  };
-
   users.users = {
     ${user.username} = {
       name = user.username;
