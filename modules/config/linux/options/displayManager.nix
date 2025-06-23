b5c9683a2f5e106dbc2b42f0aecfb74f3689a56b
InvraNet <identificationsucks@gmail.com>
2025-06-23 16:13:03 +1000

    Add: sddm greeter option

--- modules/config/linux/options/displayManager.nix
+++ modules/config/linux/options/displayManager.nix
@@ -1,9 +1,18 @@
 { unstable, system, ... }:
 {
-  services.displayManager.gdm = {
-    enable = system.greeter == "gdm";
-    wayland = true;
+  services.displayManager = {
+    gdm = {
+      enable = system.greeter == "gdm";
+      wayland = true;
+    };
+
+    sddm = {
+      enable = system.greeter == "sddm";
+      enableHidpi = true;
+      wayland.enable = true;
+    };
   };
+
   programs.sway = {
     enable = true;
     package = unstable.swayfx;
