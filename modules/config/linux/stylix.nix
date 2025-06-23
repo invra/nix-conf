354e2bd23bef5481ea81b0e90c73a14b6757ab43
Invra <identificationsucks@gmail.com>
2025-06-23 15:21:00 +1000

    Change: wallpapers

--- modules/config/linux/stylix.nix
+++ modules/config/linux/stylix.nix
@@ -3,7 +3,7 @@
   stylix = {
     enable = true;
     base16Scheme = "${unstable.base16-schemes}/share/themes/rose-pine.yaml";
-    image = unstable.lib.path.append ../../home "/programs/stylix/wallpapers/flake.png";
+    image = ../../../wallpapers/flake.jpg;
     fonts = {
       serif = {
         package = unstable.dejavu_fonts;
