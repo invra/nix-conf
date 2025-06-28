a82a9b4bd4ff98ac2f7798f54cb5420c9807d5da
Invra <identificationsucks@gmail.com>
2025-06-29 00:44:25 +1000

    Add: laptop config

---
+++ configurations/laptop_x86/default.nix
@@ -0,0 +1,65 @@
+{
+  desktop = {
+    plasma = {
+      enable = true;
+    };
+  };
+  development = {
+    git = {
+      defaultBranch = "main";
+      email = "identificationsucks@gmail.com";
+      types = [
+        "GitLab"
+        "GitHub"
+      ];
+      username = "InvraNet";
+    };
+  };
+  system = {
+    hardware-module = ./hardware-configuration.nix;
+    dock = {
+      autoHideDelay = 0.45;
+      autohide = true;
+      orientation = "right";
+    };
+    graphics = {
+      blacklists = [
+        "nouveau"
+        "nvidia"
+      ];
+      wanted = [ "amdgpu" ];
+    };
+    greeter = "gdm";
+    hostname = "NixOS";
+    interfaces = { };
+    kernelParams = [ "intel_iommu=on" ];
+    locale = "en_AU.UTF-8";
+    networking = {
+      dhcpEnabled = true;
+      firewallEnabled = false;
+      networkmanager = false;
+      # interfaces = [
+      #   {
+      #     type = "BRIDGE";
+      #     name = "br0";
+      #     interfaces = [ "enp6s0" ];
+      #     dhcpEnabled = true;
+      #   }
+      # ];
+    };
+    services = {
+      mongodb = {
+        enable = true;
+      };
+      ssh = {
+        enable = true;
+      };
+    };
+    timezone = "Australia/Sydney";
+  };
+  user = {
+    displayName = "Invraa (>.<)";
+    initialPassword = "123456";
+    username = "invra";
+  };
+}
