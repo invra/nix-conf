63542e293385a167b2529ee6d495ae32a101d0c3
InvraNet <identificationsucks@gmail.com>
2025-07-10 04:55:43 +1000

    Fix: zen-browser issues

---
+++ modules/home//programs/xdg/default.nix
@@ -0,0 +1,16 @@
+{ unstable, ... }:
+let
+  pkgs = unstable;
+in
+{
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "zen.desktop";
+      "x-scheme-handler/http" = "zen.desktop";
+      "x-scheme-handler/https" = "zen.desktop";
+      "x-scheme-handler/about" = "zen.desktop";
+      "x-scheme-handler/unknown" = "zen.desktop";
+    };
+  };
+}
