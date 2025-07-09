c56569822fb8d85f150ac5c160a6509497bf8c61
InvraNet <identificationsucks@gmail.com>
2025-07-10 05:25:35 +1000

    Add ghostty

--- modules/home//programs/xdg/default.nix
+++ modules/home//programs/xdg/default.nix
@@ -3,14 +3,28 @@ let
   pkgs = unstable;
 in
 {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "text/html" = "zen.desktop";
-      "x-scheme-handler/http" = "zen.desktop";
-      "x-scheme-handler/https" = "zen.desktop";
-      "x-scheme-handler/about" = "zen.desktop";
-      "x-scheme-handler/unknown" = "zen.desktop";
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "zen.desktop";
+        "x-scheme-handler/http" = "zen.desktop";
+        "x-scheme-handler/https" = "zen.desktop";
+        "x-scheme-handler/about" = "zen.desktop";
+        "x-scheme-handler/unknown" = "zen.desktop";
+        "application/x-desktop" = "ghostty.desktop";
+      };
+    };
+    desktopEntries = {
+      zen = {
+        name = "Zen Browser";
+        exec = "${pkgs.zen}/bin/zen";
+      };
+
+      ghostty = {
+        name = "Ghostty";
+        exec = "${pkgs.ghostty}/bin/ghostty";
+      };
     };
   };
 }
