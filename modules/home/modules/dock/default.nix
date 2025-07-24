422e9197a3930e3ffbed11bcffb5d6048fd56e4a
Invra <identificationsucks@gmail.com>
2025-07-24 20:26:18 +1000

    Reimplement default dock settings.

--- modules/home/modules/dock/default.nix
+++ modules/home/modules/dock/default.nix
@@ -1,25 +1,21 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, pkgs, ... }:
 with lib;
 let
-  cfg = config.local.dock;
-  inherit (pkgs) dockutil;
+  cfg = config.targets.darwin.dock;
+  isAppDrawerCompliant = import ./os_vers { inherit pkgs; };
+  dockutil = pkgs.dockutil;
 in
 {
   options = {
-    local.dock.enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = "Whether to set up Dock entries.";
-    };
+    targets.darwin.dock = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Whether to set up Dock entries.";
+      };
 
-    local.dock.entries = mkOption {
-      type = types.listOf (
-        types.submodule {
+      entries = mkOption {
+        type = types.listOf (types.submodule {
           options = {
             path = mkOption {
               type = types.str;
@@ -36,32 +32,69 @@ in
               description = "Extra options to pass to dockutil.";
             };
           };
-        }
-      );
-      default = [ ];
-      description = "List of Dock entries.";
+        });
+        default = [
+          {
+            path =
+              if isAppDrawerCompliant then "/System/Applications/Apps.app" else "/Applications/Launchpad.app";
+          }
+          { path = "/Applications/Safari.app"; }
+          { path = "/System/Applications/Messages.app"; }
+          { path = "/System/Applications/Mail.app"; }
+          { path = "/System/Applications/Maps.app"; }
+          { path = "/System/Applications/Photos.app"; }
+          { path = "/System/Applications/FaceTime.app"; }
+          { path = "/System/Applications/Calendar.app"; }
+          { path = "/System/Applications/Contacts.app"; }
+          { path = "/System/Applications/Reminders.app"; }
+          { path = "/System/Applications/Notes.app"; }
+          { path = "/System/Applications/Freeform.app"; }
+          { path = "/System/Applications/TV.app"; }
+          { path = "/System/Applications/Music.app"; }
+          { path = "/System/Applications/News.app"; }
+          { path = "/System/Applications/System Settings.app"; }
+        ];
+        description = "List of Dock entries.";
+      };
+    };
+
+    local.dock = mkOption {
+      type = types.attrs;
+      default = {};
+      internal = true;
     };
   };
 
-  config = mkIf cfg.enable {
-    home.activation.dockSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      (
-        haveURIs="$(${dockutil}/bin/dockutil --list | ${pkgs.coreutils}/bin/cut -f2)"
-        wantURIs="$(
-          ${lib.concatMapStringsSep "\n" (
-            entry: "echo file://$(${pkgs.coreutils}/bin/realpath '${entry.path}' | sed 's/ /%20/g')"
-          ) cfg.entries}
-        )"
+  config = mkMerge [
+    (mkIf (config.local.dock ? enable && config.local.dock.enable) {
+      assertions = [
+        {
+          assertion = false;
+          message = "local.dock is no longer supported. Please use targets.darwin.dock";
+        }
+      ];
+    })
 
-        if ! diff -wu <(echo -n "$haveURIs") <(echo -n "$wantURIs") >/dev/null 2>&1; then
-          ${dockutil}/bin/dockutil --no-restart --remove all >/dev/null 2>&1
-          ${lib.concatMapStringsSep "\n" (
-            entry:
-            "${dockutil}/bin/dockutil --replacing 'Vesktop' --no-restart --add '${entry.path}' --section ${entry.section} ${entry.options} >/dev/null 2>&1"
-          ) cfg.entries}
-          ${pkgs.killall}/bin/killall Dock >/dev/null 2>&1
-        fi
-      ) >/dev/null 2>&1
-    '';
-  };
+    (mkIf cfg.enable {
+      home.activation.dockSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        (
+          haveURIs="$(${dockutil}/bin/dockutil --list | ${pkgs.coreutils}/bin/cut -f2)"
+          wantURIs="$(
+            ${lib.concatMapStringsSep "\n" (
+              entry: "echo file://$(${pkgs.coreutils}/bin/realpath '${entry.path}' | sed 's/ /%20/g')"
+            ) cfg.entries}
+          )"
+
+          if ! diff -wu <(echo -n "$haveURIs") <(echo -n "$wantURIs") >/dev/null 2>&1; then
+            ${dockutil}/bin/dockutil --no-restart --remove all >/dev/null 2>&1
+            ${lib.concatMapStringsSep "\n" (
+              entry:
+              "${dockutil}/bin/dockutil --no-restart --add '${entry.path}' --section ${entry.section} ${entry.options} >/dev/null 2>&1"
+            ) cfg.entries}
+            ${pkgs.killall}/bin/killall Dock >/dev/null 2>&1
+          fi
+        ) >/dev/null 2>&1
+      '';
+    })
+  ];
 }
