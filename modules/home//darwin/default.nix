dc4b2082fd23148d2f431f95d81a998995b4ba4c
InvraNet <identificationsucks@gmail.com>
2025-07-24 20:26:18 +1000

    Reimplement default dock settings.

--- modules/home//darwin/default.nix
+++ modules/home//darwin/default.nix
@@ -4,102 +4,77 @@
   pkgs,
   ...
 }:
-let
-  isAppDrawerCompliant = import ../../modules/dock/os_vers {inherit pkgs;};
-in
 {
   imports = [
     ../../modules/wallpaper
     ../../modules/dock
   ];
 
-  targets.darwin.defaults = {
-    NSGlobalDomain = {
-      AppleShowAllExtensions = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticInlinePredictionEnabled = false;
-      NSAutomaticCapitalizationEnabled = false;
-      AppleICUForce24HourTime = true;
-      "com.apple.keyboard.fnState" = true;
-      AppleTemperatureUnit = "Celsius";
-      AppleMeasurementUnits = "Centimeters";
-      AppleMetricUnits = true;
-      AppleHighlightColor = "0.968627 0.831373 1.000000 Purple";
-      AppleAccentColor = 5;
+  targets.darwin = {
+    dock = {
+      enable = true;
+    } // pkgs.lib.optionalAttrs (system ? dock && system.dock ? entries) {
+      entries = system.dock.entries { inherit pkgs config; };
     };
+    
+    defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
+        NSAutomaticCapitalizationEnabled = false;
+        AppleICUForce24HourTime = true;
+        "com.apple.keyboard.fnState" = true;
+        AppleTemperatureUnit = "Celsius";
+        AppleMeasurementUnits = "Centimeters";
+        AppleMetricUnits = true;
+        AppleHighlightColor = "0.968627 0.831373 1.000000 Purple";
+        AppleAccentColor = 5;
+      };
 
-    "com.apple.screencapture" = {
-      location = "~/Desktop";
-      type = "png";
-      target = "clipboard";
-    };
+      "com.apple.screencapture" = {
+        location = "~/Desktop";
+        type = "png";
+        target = "clipboard";
+      };
 
-    "com.apple.WindowManager" = {
-      EnableStandardClickToShowDesktop = false;
-      GloballyEnabled = false;
-    };
+      "com.apple.WindowManager" = {
+        EnableStandardClickToShowDesktop = false;
+        GloballyEnabled = false;
+      };
 
-    "com.apple.menuextra.clock" = {
-      Show24Hour = true;
-      ShowSeconds = true;
-    };
+      "com.apple.menuextra.clock" = {
+        Show24Hour = true;
+        ShowSeconds = true;
+      };
 
-    "com.apple.desktopservices" = {
-      DSDontWriteNetworkStores = true;
-      DSDontWriteUSBStores = true;
-    };
+      "com.apple.desktopservices" = {
+        DSDontWriteNetworkStores = true;
+        DSDontWriteUSBStores = true;
+      };
 
-    "com.apple.finder" = {
-      AppleShowAllExtensions = true;
-      CreateDesktop = false;
-      QuitMenuItem = true;
-      NewWindowTarget = "Home";
-    };
+      "com.apple.finder" = {
+        AppleShowAllExtensions = true;
+        CreateDesktop = false;
+        QuitMenuItem = true;
+        NewWindowTarget = "Home";
+      };
 
-    "com.apple.dock" = {
-      autohide = system.dock.autohide or false;
-      orientation = system.dock.orientation or "bottom";
-      tilesize = system.dock.size or 64;
-      minimize-to-application = true;
-      show-recents = false;
-      wvous-br-corner = 1;
-      size-immutable = true;
+      "com.apple.dock" = {
+        autohide = system.dock.autohide or false;
+        orientation = system.dock.orientation or "bottom";
+        tilesize = system.dock.size or 64;
+        minimize-to-application = true;
+        show-recents = false;
+        wvous-br-corner = 1;
+        size-immutable = true;
+      };
     };
   };
 
-  local.dock = {
-    enable = true;
-    entries =
-      (system.dock.entries or (
-        { ... }:
-        [
-          {
-            path =
-              if isAppDrawerCompliant then "/System/Applications/Apps.app" else "/Applications/Launchpad.app";
-          }
-          { path = "/Applications/Safari.app"; }
-          { path = "/System/Applications/Messages.app"; }
-          { path = "/System/Applications/Mail.app"; }
-          { path = "/System/Applications/Maps.app"; }
-          { path = "/System/Applications/Photos.app"; }
-          { path = "/System/Applications/FaceTime.app"; }
-          { path = "/System/Applications/Calendar.app"; }
-          { path = "/System/Applications/Contacts.app"; }
-          { path = "/System/Applications/Reminders.app"; }
-          { path = "/System/Applications/Notes.app"; }
-          { path = "/System/Applications/Freeform.app"; }
-          { path = "/System/Applications/TV.app"; }
-          { path = "/System/Applications/Music.app"; }
-          { path = "/System/Applications/News.app"; }
-          { path = "/System/Applications/System Settings.app"; }
-        ]
-      )
-      )
-        { inherit pkgs config; };
-  };
   programs.setWallpaper = {
     enable = true;
     wallpaperPath = ../../../../wallpapers/landscape.jpg;
