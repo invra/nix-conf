fcf805cc89f22f72837a65d97a280a6fa007447f
Invra <identificationsucks@gmail.com>
2025-07-27 03:05:35 +1000

    Add: theorectically working code.

--- auto/src/nix-darwin.rs
+++ auto/src/nix-darwin.rs
@@ -14,7 +14,22 @@ use {
         },
         path::Path,
     },
+    clap::Parser,
 };
+
+
+#[derive(Debug, Clone, Copy)]
+pub enum Error {
+    IncompatibleSystem,
+    NoFlakeProvided,
+}
+
+#[derive(Parser, Debug)]
+#[command(name = "Nix Darwin Installer", about = "A Nix installer which also boostraps the configs.", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    flake: String,
+}
 
 fn is_command_available(cmd: &str) -> bool {
     Command::new("command")
@@ -40,6 +55,9 @@ fn run_after_install_command(cmd: &str) {
 
 #[cfg(target_os = "macos")]
 fn main() {
+    let args = Args::parse();
+    let flake = args.flake;
+
     tracing_subscriber::registry()
     .with(
         fmt::layer()
@@ -55,17 +73,31 @@ fn main() {
     if !Path::exists(nix_path) {
         info!("Nix is not installed. Installing Nix...");
         run_command("curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh", false);
+
+        // TODO: PLIST MANAGEMENT
     } else {
         info!("Nix is already installed. I will skip installation.");
     }
+
+    if !is_command_available("home-manager") {
+        info!("Applying nix-darwin config...");
+        run_after_install_command(format!("sudo nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake '.#{cmd}'"));
+    } else {
+        info!("The nix-darwin installation has already happened, if it hasn't... Please uninstall or dereference home-manager.");
+    }
+
+    if !is_command_available("hx") {
+        info!("Home Manager config not applied. Applying now...");
+        run_after_install_command(format!("home-manager switch --flake '.#{flake}' -b backup"));
+        
+        println!("{} Please run \"source /etc/zshrc\" to have access to Nix.", "[FINISHED]".green());
+    } else {
+        info!("The home-manager config seems to be already applied. Please use nh to rebuild.");
+    }
+
 }
 
 
-#[cfg(not(target_os = "macos"))]
-#[derive(Debug, Clone, Copy)]
-pub enum Error {
-    IncompatibleSystem,
-}
 
 #[cfg(not(target_os = "macos"))]
 fn main() -> Result<(), Error> {
