722768d4b39c2a0fb733ef4f247ba502191213cd
Invra <identificationsucks@gmail.com>
2025-07-27 00:55:49 +1000

    Add: start

--- auto/src/nix-darwin.rs
+++ auto/src/nix-darwin.rs
@@ -1,7 +1,63 @@
+use {
+    colored::Colorize,
+    tracing::info,
+    tracing_subscriber::{
+        EnvFilter,
+        fmt::{self, format},
+        prelude::__tracing_subscriber_SubscriberExt,
+        util::SubscriberInitExt,
+    },
+    std::{
+        process::{
+            ExitStatus,
+            Command,
+        },
+        path::Path,
+    },
+};
+
+fn is_command_available(cmd: &str) -> bool {
+    Command::new("command")
+        .arg("-v")
+        .arg(cmd)
+        .arg(">/dev/null 2>&1")
+        .output().unwrap().status.success()
+}
+
+fn run_command(cmd: &str, print: bool) -> std::process::Output {
+    print.then(|| info!("I a running: {cmd}"));
+    Command::new("zsh")
+        .arg("-c")
+        .arg(cmd)
+        .output()
+        .expect(format!("{cmd} Could not be ran!").as_str())
+}
+
+fn run_after_install_command(cmd: &str) {
+    let full = format!("zsh -c \"source /etc/zshrc && {cmd} \"");
+    run_command(&full, true);
+}
+
 #[cfg(target_os = "macos")]
 fn main() {
-    use colored::Colorize;
-    println!("Hello, world!");
+    tracing_subscriber::registry()
+    .with(
+        fmt::layer()
+        .with_target(false)
+        .with_level(true)
+        .without_time()
+        .with_ansi(true)
+    )
+    .with(EnvFilter::new("info"))
+    .init();
+
+    let nix_path = Path::new("/nix/var/nix/profiles/default/bin/nix");
+    if !Path::exists(nix_path) {
+        info!("Nix is not installed. Installing Nix...");
+        run_command("curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh", false);
+    } else {
+        info!("Nix is already installed. I will skip installation.");
+    }
 }
 
 
