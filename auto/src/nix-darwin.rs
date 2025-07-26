c39a0007ecac4bdb9439ed5d6f1c99d09c72109d
Invra <identificationsucks@gmail.com>
2025-07-26 16:56:27 +1000

    Add: start

--- auto/src/nix-darwin.rs
+++ auto/src/nix-darwin.rs
@@ -1,3 +1,14 @@
+use colored::Colorize;
+use clap::Parser;
+
+#[cfg(target_os = "macos")]
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[cfg(not(target_os = "macos"))]
+fn main() -> Result<(), ()> {
+    eprintln!("{} {}", "[ERROR]".red(), "This tool only works on macOS, and should only be run on it.");
+    Err(())
 }
