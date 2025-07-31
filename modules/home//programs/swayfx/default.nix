feefdca14a0720e768c7cc3eaf7c5e0afc675d1d
InvraNet <identificationsucks@gmail.com>
2025-07-31 18:24:30 +1000

    Add wallpaper config

--- modules/home//programs/swayfx/default.nix
+++ modules/home//programs/swayfx/default.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  configTOML,
   ...
 }:
 {
@@ -7,7 +8,7 @@
     file = {
       ".config/sway/config".source = ./config;
       ".config/sway/screenshot.nu".text = import ./scripts/screenshot.nix;
-      ".config/sway/wallpaper.png".source = ../../../../../wallpapers/flake.jpg;
+      ".config/sway/wallpaper.png".source = configTOML.user.wallpaper or ../../../../../wallpapers/flake.jpg;
     };
 
     packages = with pkgs; [
