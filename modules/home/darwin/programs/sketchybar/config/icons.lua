0e37aa103780d4f225a8a8041b78fb91eab526f3
Invra <identificationsucks@gmail.com>
2025-08-05 12:49:48 +1000

    Disable configuring of icon type

--- modules/home/darwin/programs/sketchybar/config/icons.lua
+++ modules/home/darwin/programs/sketchybar/config/icons.lua
@@ -1,48 +1,6 @@
 local settings = require("settings")
 
 local icons = {
-  sf_symbols = {
-    plus = "􀅼",
-    loading = "􀖇",
-    apple = "",
-    gear = "􀍟",
-    cpu = "􀫥",
-    clipboard = "􀉄",
-
-    switch = {
-      on = "􁏮",
-      off = "􁏯",
-    },
-    volume = {
-      _100="􀊩",
-      _66="􀊧",
-      _33="􀊥",
-      _10="􀊡",
-      _0="􀊣",
-    },
-    battery = {
-      _100 = "􀛨",
-      _75 = "􀺸",
-      _50 = "􀺶",
-      _25 = "􀛩",
-      _0 = "􀛪",
-      charging = "􀢋"
-    },
-    wifi = {
-      upload = "􀄨",
-      download = "􀄩",
-      connected = "􀙇",
-      disconnected = "􀙈",
-      router = "􁓤",
-    },
-    media = {
-      back = "􀊊",
-      forward = "􀊌",
-      play_pause = "􀊈",
-    },
-  },
-
-  -- Alternative NerdFont icons
   nerdfont = {
     plus = "",
     loading = "",
@@ -85,8 +43,4 @@ local icons = {
   },
 }
 
-if not (settings.icons == "nerd") then
-  return icons.sf_symbols
-else
-  return icons.nerdfont
-end
+return icons.nerdfont
