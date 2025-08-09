dd9abed16f7586a7b7598a05077e8651d6529af9
Invra <identificationsucks@gmail.com>
2025-08-09 19:10:13 +1000

    Remove helpers folder.

--- modules/home/darwin/programs/sketchybar/config/settings.lua
+++ modules/home/darwin/programs/sketchybar/config/settings.lua
@@ -2,5 +2,16 @@ return {
   paddings = 3,
   group_paddings = 5,
 
-  font = require("helpers.default_font"),
+  font = {
+    text = "JetBrainsMono Nerd Font",
+    numbers = "JetBrainsMono Nerd Font",
+
+    style_map = {
+      ["Regular"] = "Regular",
+      ["Semibold"] = "Semibold",
+      ["Bold"] = "Bold",
+      ["Heavy"] = "Heavy",
+      ["Black"] = "Black",
+    },
+  },
 }
