dcb0b08002112b52749ca97e9dacf7d55d4d2f8e
Invra <identificationsucks@gmail.com>
2025-08-09 15:32:47 +1000

    Fix: unsupported system issue Isuue has been fixed by using the `sysinfo` crate instead of `systemstat`

--- modules/home/darwin/programs/sketchybar/packages/cpu_load/src/main.rs
+++ modules/home/darwin/programs/sketchybar/packages/cpu_load/src/main.rs
@@ -1,15 +1,13 @@
 use {
     std::{env, io, thread::sleep, time::Duration},
-    systemstat::{CPULoad, Platform, System},
+    sysinfo::System,
 };
 
 #[cfg(target_os = "macos")]
-fn run_sketchybar(event_name: &str, user: f32, sys: f32, total: f32) -> io::Result<()> {
-    std::proccess::Command::new("sketchybar")
+fn run_sketchybar(event_name: &str, total: f32) -> io::Result<()> {
+    std::process::Command::new("sketchybar")
         .arg("--trigger")
         .arg(event_name)
-        .arg(format!("user_load={user:0>.0}"))
-        .arg(format!("sys_load={sys:0>.0}"))
         .arg(format!("total_load={total:0>.0}"))
         .status()
         .map(std::mem::drop)
@@ -17,18 +15,18 @@ fn run_sketchybar(event_name: &str, user: f32, sys: f32, total: f32) -> io::Resu
 }
 
 #[cfg(not(target_os = "macos"))]
-fn run_sketchybar(event_name: &str, user: f32, sys: f32, total: f32) -> io::Result<()> {
-    println!("Event: {event_name}, User: {user:0>.0}%, System: {sys:0>.0}%, Total: {total:0>.0}%");
+fn run_sketchybar(event_name: &str, total: f32) -> io::Result<()> {
+    println!("Event: {event_name}, Total CPU: {total:0>.0}%");
     Ok(())
 }
+
 fn main() -> Result<(), &'static str> {
     let args: Vec<String> = env::args().collect();
-    let sys = System::new();
 
     let (event_name, freq) = if args.len() == 3 {
         (
             args[1].as_str(),
-            args[2].parse::<f64>().map_err(|_| "Invalid frequency")?,
+            args[2].parse::<u64>().map_err(|_| "Invalid frequency")?,
         )
     } else {
         println!("Usage:");
@@ -39,20 +37,14 @@ fn main() -> Result<(), &'static str> {
         return Ok(());
     };
 
+    let mut sys = System::new();
+
     loop {
-        _ = sys
-            .cpu_load_aggregate()
-            .inspect(|_| sleep(Duration::from_secs_f64(freq)))
-            .and_then(|x| x.done())
-            .map(|x| dbg!(x))
-            .inspect_err(|e| eprintln!("Couldn't get cpu stats, {e}"))
-            .and_then(|CPULoad { user, system, .. }| {
-                run_sketchybar(
-                    event_name,
-                    user * 100.0,
-                    system * 100.0,
-                    (user + system) * 100.0,
-                )
-            })
+        sys.refresh_cpu_usage();
+        let total_cpu = sys.global_cpu_usage();
+
+        let _ = run_sketchybar(event_name, total_cpu);
+
+        sleep(Duration::from_secs(freq));
     }
 }
