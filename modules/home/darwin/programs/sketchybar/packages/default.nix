0770ef163c57c24e537d0d94054495761f0771fc
Invra <identificationsucks@gmail.com>
2025-08-09 19:22:06 +1000

    add a `with pkgs;`

--- modules/home/darwin/programs/sketchybar/packages/default.nix
+++ modules/home/darwin/programs/sketchybar/packages/default.nix
@@ -2,22 +2,23 @@
   pkgs,
   ...
 }:
+with pkgs;
 {
-  cpu_load = pkgs.rustPlatform.buildRustPackage {
+  cpu_load = rustPlatform.buildRustPackage {
     pname = "cpu_load";
     version = "0.1.0";
     src = ./cpu_load;
     cargoLock.lockFile = ./cpu_load/Cargo.lock;
   };
 
-  network_load = pkgs.rustPlatform.buildRustPackage {
+  network_load = rustPlatform.buildRustPackage {
     pname = "network_load";
     version = "0.1.0";
     src = ./network_load;
     cargoLock.lockFile = ./network_load/Cargo.lock;
   };
 
-  menus = pkgs.rustPlatform.buildRustPackage {
+  menus = rustPlatform.buildRustPackage {
     pname = "menus";
     version = "0.1.0";
     src = ./menus;
