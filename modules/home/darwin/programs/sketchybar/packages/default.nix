1c90638ff130accccdaed08818d2af2e9c9c3f58
Invra <identificationsucks@gmail.com>
2025-08-09 17:18:26 +1000

    A kind message <3

---
+++ modules/home/darwin/programs/sketchybar/packages/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+let
+  cpu_load = pkgs.rustPlatform.buildRustPackage {
+    pname = "cpu_load";
+    version = "0.1.0";
+    src = ./cpu_load;
+    cargoLock.lockFile = ./cpu_load/Cargo.lock;
+  };
+
+  network_load = pkgs.rustPlatform.buildRustPackage {
+    pname = "network_load";
+    version = "0.1.0";
+    src = ./network_load;
+    cargoLock.lockFile = ./network_load/Cargo.lock;
+  };
+in {
+  inherit cpu_load network_load;
+}
--- modules/home/darwin/programs/sketchybar/packages/network_load/src/main.rs
+++ modules/home/darwin/programs/sketchybar/packages/network_load/src/main.rs
@@ -1,72 +1,84 @@
-use {
-    std::{env, io, thread::sleep, time::Duration},
-    systemstat::{ByteSize, NetworkStats, Platform, System},
-};
-
-#[cfg(not(target_os = "macos"))]
-fn run_sketchybar(event_name: &str, upload: String, dload: String) -> io::Result<()> {
-    println!("Event: {event_name}, upload: {upload}, dload: {dload}");
-    Ok(())
-}
+use netdev::get_interfaces;
+use std::{env, io, thread::sleep, time::Duration};
 
 #[cfg(target_os = "macos")]
 fn run_sketchybar(event_name: &str, upload: String, dload: String) -> io::Result<()> {
-    std::process::Command::new("sketchybar")
+    match std::process::Command::new("sketchybar")
         .arg("--trigger")
         .arg(event_name)
         .arg(format!("upload={upload}"))
         .arg(format!("download={dload}"))
         .status()
-        .map(std::mem::drop)
-        .inspect_err(|e| eprintln!("Failed to run sketchybar: {}", e))
+    {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            eprintln!("Failed to run sketchybar: {}", e);
+            Err(e)
+        }
+    }
 }
 
-fn main() -> Result<(), &'static str> {
-    let args: Vec<String> = env::args().collect();
-    let sys = System::new();
+#[cfg(not(target_os = "macos"))]
+fn run_sketchybar(event_name: &str, upload: String, dload: String) -> io::Result<()> {
+    println!("Event: {event_name}, upload: {upload}, download: {dload}");
+    Ok(())
+}
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
     let (iname, event_name, freq) = if args.len() == 4 {
-        (
-            args[1].as_str(),
-            args[2].as_str(),
-            args[3].parse::<f64>().map_err(|_| "Invalid frequency")?,
-        )
+        (args[1].as_str(), args[2].as_str(), args[3].parse::<f64>()?)
     } else {
-        println!("Usage:");
         println!(
-            "  {} <interface-name> <event-name> <freq-seconds>       # run mode (send to sketchybar)",
+            "Usage: {} <interface-name> <event-name> <freq-seconds>",
             args[0]
         );
         return Ok(());
     };
 
-    let NetworkStats {
-        rx_bytes: mut prev_rx_bytes,
-        tx_bytes: mut prev_tx_bytes,
-        ..
-    } = sys.network_stats(iname).unwrap();
+    let mut prev_rx;
+    let mut prev_tx;
+
+    if let Some(mut iface) = get_interfaces().into_iter().find(|i| i.name == iname) {
+        iface.update_stats()?;
+        if let Some(stats) = iface.stats {
+            prev_rx = stats.rx_bytes;
+            prev_tx = stats.tx_bytes;
+        } else {
+            return Err("No stats available".into());
+        }
+    } else {
+        return Err("Interface not found".into());
+    }
+
     loop {
         sleep(Duration::from_secs_f64(freq));
-        _ = sys
-            .network_stats(iname)
-            .map(|x| dbg!(x))
-            .inspect_err(|e| eprintln!("Couldn't get cpu stats, {e}"))
-            .and_then(
-                |NetworkStats {
-                     rx_bytes, tx_bytes, ..
-                 }| {
-                    let (rx, tx) = (
-                        rx_bytes.as_u64() - prev_rx_bytes.as_u64(),
-                        tx_bytes.as_u64() - prev_tx_bytes.as_u64(),
-                    );
-                    prev_rx_bytes = rx_bytes;
-                    prev_tx_bytes = tx_bytes;
-                    run_sketchybar(
-                        event_name,
-                        ByteSize::b(rx).to_string(),
-                        ByteSize::b(tx).to_string(),
-                    )
-                },
-            )
+
+        if let Some(mut iface) = get_interfaces().into_iter().find(|i| i.name == iname) {
+            iface.update_stats()?;
+            if let Some(stats) = iface.stats {
+                let rx = stats.rx_bytes - prev_rx;
+                let tx = stats.tx_bytes - prev_tx;
+                prev_rx = stats.rx_bytes;
+                prev_tx = stats.tx_bytes;
+
+                run_sketchybar(
+                    event_name,
+                    format_bps((tx as f64 / freq) as u64),
+                    format_bps((rx as f64 / freq) as u64),
+                )?;
+            }
+        }
     }
+}
+
+fn format_bps(bytes_per_second: u64) -> String {
+    let mut bits_per_second = bytes_per_second as f64 * 8.0;
+    const UNITS: [&str; 5] = ["Bps", "Kbps", "Mbps", "Gbps", "Tbps"];
+    let mut unit = 0;
+    while bits_per_second >= 1000.0 && unit < UNITS.len() - 1 {
+        bits_per_second /= 1000.0;
+        unit += 1;
+    }
+    format!("{:0>3.0} {}", bits_per_second, UNITS[unit])
 }
