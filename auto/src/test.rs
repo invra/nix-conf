014fef95747ab64831b76736403d5365422a68d1
Invra <identificationsucks@gmail.com>
2025-08-13 18:04:15 +1000

    Add: initial

--- auto/src/test.rs
+++ auto/src/test.rs
@@ -1,5 +1,20 @@
-use colored::Colorize;
+use {
+    colored::Colorize,
+    os_info::{Version, get as get_os_info},
+};
 
 fn main() {
-    println!("{} {} {} {}","Hello!".bold().green(), "If you see".yellow(), "this message".italic().bold().yellow(), "the toolchain is working correctly!".yellow())
+    println!(
+        "{} Running some stuff to get your system output to test if everything dectects correctly.",
+        "[INFO]".yellow()
+    );
+
+    match get_os_info().version() {
+        &Version::Semantic(major, _, _) => {
+            println!("{} macOS Version: {major}", "[SYSTEM]".green())
+        }
+        _ => println!("cow"),
+    }
+
+    println!("{} macOS Name: {:?}", "[SYSTEM]".green(), whoami::distro());
 }
