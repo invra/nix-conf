670f050f4b620a1c29695897ac95bc60859e30ac
Hiten Tandon <tandonhiten@gamil.com>
2025-08-19 14:40:13 +0530

    Chore: Refactor

--- modules/home/common/programs/custom/gc/src/main.rs
+++ modules/home/common/programs/custom/gc/src/main.rs
@@ -1,9 +1,12 @@
-use anyhow::{Context, Result};
-use clap::Parser;
-use git2::{Cred, FetchOptions, RemoteCallbacks, Repository};
-use rpassword::prompt_password;
-use std::fs;
-use std::path::{Path, PathBuf};
+use {
+    clap::Parser,
+    git2::{Cred, FetchOptions, RemoteCallbacks, Repository},
+    rpassword::prompt_password,
+    std::{
+        fs,
+        path::{Path, PathBuf},
+    },
+};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,39 +22,39 @@ struct Args {
 }
 
 fn candidate_identities() -> Vec<PathBuf> {
-    let mut ids = Vec::new();
-    if let Ok(home) = std::env::var("HOME") {
-        let ssh_dir = Path::new(&home).join(".ssh");
-        if let Ok(entries) = fs::read_dir(&ssh_dir) {
-            for entry in entries.flatten() {
-                let path = entry.path();
-                if !path.is_file() {
-                    continue;
-                }
+    let mut ids = vec![];
+    let Ok(home) = std::env::var("HOME") else {
+        return ids;
+    };
+    let ssh_dir = Path::new(&home).join(".ssh");
+    let Ok(entries) = fs::read_dir(&ssh_dir) else {
+        return ids;
+    };
 
-                if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
-                    if name.ends_with(".pub") || name == "config" || name.starts_with("known_hosts")
-                    {
-                        continue;
-                    }
-
-                    ids.push(path);
-                }
-            }
+    for path in entries.flatten().map(|x| x.path()) {
+        if !path.is_file() {
+            continue;
         }
+
+        let Some(name) = path.file_name().and_then(|n| n.to_str()) else {
+            continue;
+        };
+
+        if name.ends_with(".pub") || name == "config" || name.starts_with("known_hosts") {
+            continue;
+        }
+
+        ids.push(path);
     }
     ids
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), String> {
     let args = Args::parse();
 
     let parts: Vec<&str> = args.source.split(':').collect();
     if parts.len() < 2 {
-        anyhow::bail!(
-            "Invalid repo format: {}. Use github:user/repo or ssh:gitlab:user/repo",
-            args.source
-        );
+        return Err("Not enough arguments".into());
     }
 
     let is_ssh = parts[0] == "ssh";
@@ -69,17 +72,17 @@ fn main() -> Result<()> {
 
     let url = if is_ssh {
         match provider.to_lowercase().as_str() {
-            "github" => format!("git@github.com:{repo}"),
-            "gitlab" => format!("git@gitlab.com:{repo}"),
-            "codeberg" => format!("git@codeberg.org:{repo}"),
-            _ => anyhow::bail!("Unsupported SSH provider: {provider}"),
+            "github" | "gh" => format!("git@github.com:{repo}"),
+            "gitlab" | "gl" => format!("git@gitlab.com:{repo}"),
+            "codeberg" | "cb" => format!("git@codeberg.org:{repo}"),
+            _ => Err(format!("Unsupported SSH provider: {provider}"))?,
         }
     } else {
         match provider.to_lowercase().as_str() {
-            "github" => format!("https://github.com/{repo}"),
-            "gitlab" => format!("https://gitlab.com/{repo}"),
-            "codeberg" => format!("https://codeberg.org:{repo}"),
-            _ => anyhow::bail!("Unsupported provider: {provider}"),
+            "github" | "gh" => format!("https://github.com/{repo}"),
+            "gitlab" | "gl" => format!("https://gitlab.com/{repo}"),
+            "codeberg" | "cb" => format!("https://codeberg.org:{repo}"),
+            _ => Err(format!("Unsupported provider: {provider}"))?,
         }
     };
 
@@ -90,17 +93,16 @@ fn main() -> Result<()> {
     println!("Cloning from {} into {}", url, target_dir.display());
 
     if is_ssh {
-        let identities = if let Some(file) = args.identity_file {
-            vec![file]
-        } else {
-            candidate_identities()
-        };
+        let identities = args
+            .identity_file
+            .map(|x| vec![x])
+            .unwrap_or_else(candidate_identities);
 
         let mut callbacks = RemoteCallbacks::new();
-        callbacks.credentials(move |_url, username_from_url, _allowed_types| {
+        callbacks.credentials(move |_, username_from_url, _allowed_types| {
             let username = username_from_url.unwrap_or("git");
 
-            for id in &identities {
+            for id in identities.iter() {
                 println!("Trying SSH key: {}", id.display());
                 let passphrase =
                     prompt_password(&format!("Enter passphrase for key {}: ", id.display()))
@@ -126,14 +128,14 @@ fn main() -> Result<()> {
 
         builder
             .clone(&url, &target_dir)
-            .with_context(|| format!("Failed to clone {url} into {}", target_dir.display()))?;
+            .map_err(|_| format!("Failed to clone {url} into {}", target_dir.display()))?;
     } else {
         Repository::clone(&url, &target_dir)
-            .with_context(|| format!("Failed to clone {url} into {}", target_dir.display()))?;
+            .map_err(|_| format!("Failed to clone {url} into {}", target_dir.display()))?;
     }
 
     if args.cd {
-        println!("Would cd into {}", target_dir.display());
+        println!("Run `cd {}`", target_dir.display());
     }
 
     Ok(())
