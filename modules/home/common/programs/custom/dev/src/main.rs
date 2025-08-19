863f92096dc54f1e1fd4bedb592e9fcfcc166fa5
Hiten Tandon <tandonhiten@gamil.com>
2025-08-19 14:40:32 +0530

    Chore: Refactor

--- modules/home/common/programs/custom/dev/src/main.rs
+++ modules/home/common/programs/custom/dev/src/main.rs
@@ -1,17 +1,24 @@
-use std::process::{Command, Stdio};
+use std::{
+    io,
+    process::{Command, Stdio},
+};
 
-fn main() -> anyhow::Result<()> {
-    let shell = std::env::var("SHELL").unwrap_or("/bin/bash".to_string());
-
-    let shell = shell.clone();
-
-    let mut child = Command::new("nix")
-        .args(["develop", "--command", &shell])
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .spawn()?;
-
-    let status = child.wait()?;
-    std::process::exit(status.code().unwrap_or(1));
+fn main() -> io::Result<()> {
+    std::process::exit(
+        Command::new("nix")
+            .args([
+                "develop",
+                "--command",
+                std::env::var("SHELL")
+                    .unwrap_or("/bin/bash".into())
+                    .as_str(),
+            ])
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()?
+            .wait()?
+            .code()
+            .ok_or(io::ErrorKind::Interrupted)?,
+    );
 }
