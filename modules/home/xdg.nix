e178ed564e69aa49c4714b8baa669d6dbb5d6aa0
Invra <identificationsucks@gmail.com>
2025-09-02 22:56:11 +1000

    add: xdg

---
+++ modules/home/xdg.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
+  xdg = lib.mkIf pkgs.stdenv.isLinux {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "zen.desktop";
+        "x-scheme-handler/http" = "zen.desktop";
+        "x-scheme-handler/https" = "zen.desktop";
+        "x-scheme-handler/about" = "zen.desktop";
+        "x-scheme-handler/unknown" = "zen.desktop";
+        "application/x-desktop" = "ghostty.desktop";
+      };
+    };
+    desktopEntries = with pkgs; {
+      ghostty = {
+        name = "Ghostty";
+        exec = "${ghostty}/bin/ghostty";
+      };
+    };
+  };
+}
+
