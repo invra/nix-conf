f86f84061c480112711803552b3cb1b0912f5390
Invra <identificationsucks@gmail.com>
2025-09-02 23:52:04 +1000

    Add swayfx

---
+++ modules/home/swayfx/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  pkgs,
+  linux,
+  flakeConfig,
+  ...
+}:
+{
+  home = lib.optionalAttrs linux {
+    file = {
+      ".config/sway/config".source = import ./config.nix;
+      ".config/sway/screenshot.nu".text = import ./scripts/screenshot.nix;
+      ".config/sway/wallpaper.png".source =
+        flakeConfig.user.wallpaper or ../../wallpapers/flake.jpg;
+    };
+
+    packages = with pkgs; [
+      playerctl
+      wl-clipboard
+      grim
+      slurp
+    ];
+
+    sessionVariables.NIXOS_OZONE_WL = "1";
+  };
+}
+
