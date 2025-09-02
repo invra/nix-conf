2e9229c9ead47a2e46ea22069b942e0338e20728
Invra <identificationsucks@gmail.com>
2025-09-02 23:29:33 +1000

    Add fuzzel

---
+++ modules/home/fuzzel.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  pkgs,
+  linux,
+  ...
+}:
+{
+  stylix.targets.fuzzel.enable = false;
+  programs.fuzzel = lib.optionalAttrs linux {
+    enable = true;
+    settings = {
+      main = {
+        terminal = "${pkgs.ghostty}/bin/ghostty";
+        width = 60;
+      };
+      colors = {
+        background = "1f1d2eff";
+        text = "6e6a86ff";
+        prompt = "6e6a86ff";
+        placeholder = "908caaff";
+        input = "e0def4ff";
+        match = "9ccfd8ff";
+        selection-text = "908caaff";
+        selection = "26233aff";
+        selection-match = "9ccfd8ff";
+        counter = "c4a7e7ff";
+        border = "ebbcbaff";
+      };
+    };
+  };
+}
+
