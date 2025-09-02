36307c48b4007678fb9a93e695aec8a96c24dda5
Invra <identificationsucks@gmail.com>
2025-09-02 23:29:28 +1000

    Ad: stylix

---
+++ modules/home/stylix.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  pkgs,
+  linux,
+  flakeConfig,
+  ...
+}:
+{
+  stylix = lib.optionalAttrs linux {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    image = flakeConfig.user.wallpaper or ../../../../wallpapers/flake.jpg;
+    icons = {
+      enable = true;
+      dark = "Papirus-Dark";
+      light = "Papirus-Light";
+      package = pkgs.papirus-icon-theme;
+    };
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 24;
+    };
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrains Mono Nerd Font";
+      };
+    };
+  };
+}
