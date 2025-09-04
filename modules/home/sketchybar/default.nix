260077cb29e337aa1b62d80f682b6e4579a8595d
Invra <identificationsucks@gmail.com>
2025-09-04 14:53:33 +1000

    fold let binding

--- modules/home/sketchybar/default.nix
+++ modules/home/sketchybar/default.nix
@@ -6,34 +6,32 @@
 }:
 
 let
-  helpers = {
-    cpu_load = pkgs.rustPlatform.buildRustPackage {
-      pname = "cpu_load";
-      version = "0.1.0";
-      src = ./packages/cpu_load;
-      cargoLock.lockFile = ./packages/cpu_load/Cargo.lock;
-    };
+  cpu_load = pkgs.rustPlatform.buildRustPackage {
+    pname = "cpu_load";
+    version = "0.1.0";
+    src = ./packages/cpu_load;
+    cargoLock.lockFile = ./packages/cpu_load/Cargo.lock;
+  };
 
-    network_load = pkgs.rustPlatform.buildRustPackage {
-      pname = "network_load";
-      version = "0.1.0";
-      src = ./packages/network_load;
-      cargoLock.lockFile = ./packages/network_load/Cargo.lock;
-    };
+  network_load = pkgs.rustPlatform.buildRustPackage {
+    pname = "network_load";
+    version = "0.1.0";
+    src = ./packages/network_load;
+    cargoLock.lockFile = ./packages/network_load/Cargo.lock;
+  };
 
-    memory_load = pkgs.rustPlatform.buildRustPackage {
-      pname = "memory_load";
-      version = "0.1.0";
-      src = ./packages/memory_load;
-      cargoLock.lockFile = ./packages/memory_load/Cargo.lock;
-    };
+  memory_load = pkgs.rustPlatform.buildRustPackage {
+    pname = "memory_load";
+    version = "0.1.0";
+    src = ./packages/memory_load;
+    cargoLock.lockFile = ./packages/memory_load/Cargo.lock;
+  };
 
-    menus = pkgs.rustPlatform.buildRustPackage {
-      pname = "menus";
-      version = "0.1.0";
-      src = ./packages/menus;
-      cargoLock.lockFile = ./packages/menus/Cargo.lock;
-    };
+  menus = pkgs.rustPlatform.buildRustPackage {
+    pname = "menus";
+    version = "0.1.0";
+    src = ./packages/menus;
+    cargoLock.lockFile = ./packages/menus/Cargo.lock;
   };
 in
 lib.optionalAttrs (!linux) {
@@ -48,7 +46,7 @@ lib.optionalAttrs (!linux) {
     };
     includeSystemPath = true;
 
-    extraPackages = with helpers; [
+    extraPackages = [
       pkgs.aerospace
       network_load
       memory_load
