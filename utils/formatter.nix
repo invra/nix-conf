7a036e26d38ceab0c9915528d104fce8b58a5352
Invra <identificationsucks@gmail.com>
2025-09-10 19:07:03 +1000

    Remove stylua

--- utils/formatter.nix
+++ utils/formatter.nix
@@ -14,17 +14,6 @@ let
         shellcheck.enable = true;
         shfmt.enable = true;
         swift-format.enable = true;
-
-        stylua = {
-          enable = true;
-          settings = {
-            indent_type = "Spaces";
-            indent_width = 2;
-            quote_style = "ForceDouble";
-            call_parentheses = "Always";
-            sort_requires.enabled = true;
-          };
-        };
       };
     }).config.build;
 in
