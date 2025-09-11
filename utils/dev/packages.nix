8449200a0cb91dc905ce19ae5274feae67046a72
Invra <identificationsucks@gmail.com>
2025-09-12 03:20:51 +1000

    Add: packages for the automation

--- utils/dev/packages.nix
+++ utils/dev/packages.nix
@@ -6,6 +6,27 @@
 let
   dockPkgs = import ../../modules/dock/packages.nix { inherit pkgs; };
   dock = dockPkgs.dock;
-in {
-  inherit dock;
+
+  auto = {
+    test = pkgs.rustPlatform.buildRustPackage {
+      pname = "test";
+      version = "0.1.0";
+      src = ../../auto;
+      cargoLock = {
+        lockFile = ../../auto/Cargo.lock;
+      };
+    };
+
+    bootstrap-darwin = pkgs.rustPlatform.buildRustPackage {
+      pname = "bootstrap-darwin";
+      version = "0.1.0";
+      src = ../../auto;
+      cargoLock = {
+        lockFile = ../../auto/Cargo.lock;
+      };
+    };
+  };
+in 
+{
+  inherit dock auto;
 }
