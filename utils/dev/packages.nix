a1f032f072cbda3fb0cd5899f838a6ba46435c07
Invra <identificationsucks@gmail.com>
2025-09-16 16:26:14 +1000

    remove inactive

--- utils/dev/packages.nix
+++ utils/dev/packages.nix
@@ -5,7 +5,6 @@
 let
   dockPkgs = import ../../modules/dock/packages.nix { inherit pkgs; };
   dock = dockPkgs.dock;
-  # buildRustPackage = pkgs.rustPlatform.buildRustPackage;
 in
 {
   inherit dock;
