b21de4080953944e617b7b23c0a7c3944b1b907d
Invra <identificationsucks@gmail.com>
2025-11-04 00:22:43 +1100

    fix error

--- modules/config/services.nix
+++ modules/config/services.nix
@@ -12,85 +12,83 @@ in
     tailscale.enable = true;
   }
   // lib.optionalAttrs linux {
-    services = {
-      flatpak.enable = true;
-      blueman.enable = true;
-      tailscale.enable = true;
-      qemuGuest.enable = true;
-      spice-vdagentd.enable = true;
-      xserver = {
-        enable = true;
-        videoDrivers = system.graphics.wanted;
+    flatpak.enable = true;
+    blueman.enable = true;
+    tailscale.enable = true;
+    qemuGuest.enable = true;
+    spice-vdagentd.enable = true;
+    xserver = {
+      enable = true;
+      videoDrivers = system.graphics.wanted;
+    };
+
+    desktopManager.plasma6.enable = desktop.plasma.enable;
+    fwupd.enable = true;
+    pulseaudio.daemon.config.flat-volumes = "no";
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+      jack.enable = true;
+
+      extraConfig.pipewire."91-null-sinks" = {
+        "context.objects" = [
+          {
+            factory = "spa-node-factory";
+            args = {
+              "factory.name" = "support.node.driver";
+              "node.name" = "Dummy-Driver";
+              "priority.driver" = 8000;
+            };
+          }
+          {
+            factory = "adapter";
+            args = {
+              "factory.name" = "support.null-audio-sink";
+              "node.name" = "Microphone-Proxy";
+              "node.description" = "Microphone";
+              "media.class" = "Audio/Source/Virtual";
+              "audio.position" = "FL,FR";
+            };
+          }
+          {
+            factory = "adapter";
+            args = {
+              "factory.name" = "support.null-audio-sink";
+              "node.name" = "Main-Output-Proxy";
+              "node.description" = "Main Output";
+              "media.class" = "Audio/Sink";
+              "audio.position" = "FL,FR";
+            };
+          }
+        ];
       };
+    };
 
-      desktopManager.plasma6.enable = desktop.plasma.enable;
-      fwupd.enable = true;
-      pulseaudio.daemon.config.flat-volumes = "no";
-      pipewire = {
-        enable = true;
-        alsa.enable = true;
-        pulse.enable = true;
-        jack.enable = true;
-
-        extraConfig.pipewire."91-null-sinks" = {
-          "context.objects" = [
-            {
-              factory = "spa-node-factory";
-              args = {
-                "factory.name" = "support.node.driver";
-                "node.name" = "Dummy-Driver";
-                "priority.driver" = 8000;
-              };
-            }
-            {
-              factory = "adapter";
-              args = {
-                "factory.name" = "support.null-audio-sink";
-                "node.name" = "Microphone-Proxy";
-                "node.description" = "Microphone";
-                "media.class" = "Audio/Source/Virtual";
-                "audio.position" = "FL,FR";
-              };
-            }
-            {
-              factory = "adapter";
-              args = {
-                "factory.name" = "support.null-audio-sink";
-                "node.name" = "Main-Output-Proxy";
-                "node.description" = "Main Output";
-                "media.class" = "Audio/Sink";
-                "audio.position" = "FL,FR";
-              };
-            }
-          ];
+    libinput.enable = true;
+    openssh.enable = true;
+    samba = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        global = {
+          securityType = "user";
+          "workgroup" = "IDALON";
+          "server string" = "Main SMB";
+          "netbios name" = "smbnix";
+          "security" = "user";
+          "guest account" = "nobody";
+          "map to guest" = "bad user";
         };
-      };
-
-      libinput.enable = true;
-      openssh.enable = true;
-      samba = {
-        enable = true;
-        openFirewall = true;
-        settings = {
-          global = {
-            securityType = "user";
-            "workgroup" = "IDALON";
-            "server string" = "Main SMB";
-            "netbios name" = "smbnix";
-            "security" = "user";
-            "guest account" = "nobody";
-            "map to guest" = "bad user";
-          };
-          "private" = {
-            "path" = "/srv/smb";
-            "browseable" = "yes";
-            "read only" = "no";
-            "guest ok" = "no";
-            "create mask" = "0644";
-            "directory mask" = "0755";
-            "force user" = "invra";
-            "force group" = "wheel";
-          };
+        "private" = {
+          "path" = "/srv/smb";
+          "browseable" = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force user" = "invra";
+          "force group" = "wheel";
         };
       };
     };
