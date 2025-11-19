de851276dbfa818135708a9651f0cc0935655faa
Invra <identificationsucks@gmail.com>
2025-11-19 15:19:03 +1100

    remove nixos module for steam

--- modules/config/packages.nix
+++ modules/config/packages.nix
@@ -43,12 +43,6 @@
 
   programs = lib.optionalAttrs linux {
     nix-ld.enable = true;
-    steam = {
-      enable = (!pkgs.stdenv.isx86_64);
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-    };
     virt-manager.enable = true;
     dconf.enable = true;
     thunar.enable = true;
