52253f521904b16868ff01c409e9ad31b9e611b3
Invra <identificationsucks@gmail.com>
2025-12-05 23:32:59 +1100

    add a tiny emacs config

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -2,7 +2,27 @@
   ...
 }:
 {
+  services.emacs.enable = false;
+
   programs.emacs = {
     enable = true;
+  	extraPackages = epkgs: [
+  	  epkgs.evil
+  	];
+  	extraConfig = ''
+      (tool-bar-mode 0)
+      (scroll-bar-mode 0)
+      (menu-bar-mode 0)
+
+      (setq inhibit-startup-message t) 
+      (setq initial-scratch-message nil)
+
+      (setq make-backup-files nil)
+
+      (use-package evil
+        :init
+        (setq evil-want-keybinding nil)
+        (evil-mode 1))
+    '';
   };
 }
