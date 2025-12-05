b87e78741ff85ae5a4002458c44c0aca462bbb71
Invra <identificationsucks@gmail.com>
2025-12-05 23:46:59 +1100

    add dashboard

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -8,14 +8,22 @@
     enable = true;
   	extraPackages = epkgs: [
   	  epkgs.evil
+  	  epkgs.dashboard
+  	  epkgs.all-the-icons
   	];
   	extraConfig = ''
+  	  (require 'dashboard)
+      (dashboard-setup-startup-hook)
+      (setq dashboard-banner-logo-title "Welcome to Emacs")
+      (setq dashboard-center-content t)
+      (setq dashboard-vertically-center-content t)
+      (setq dashboard-icon-type 'all-the-icons)
+      (setq inhibit-splash-screen t)
+      (setq inhibit-startup-message t)
+
       (tool-bar-mode 0)
       (scroll-bar-mode 0)
       (menu-bar-mode 0)
-
-      (setq inhibit-startup-message t) 
-      (setq initial-scratch-message nil)
 
       (setq make-backup-files nil)
 
