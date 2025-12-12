c635b7fa4868f2332aa99850653c1fcca074b584
Invra <identificationsucks@gmail.com>
2025-12-12 20:14:34 +1100

    manage homedir folders

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   lib,
   ...
@@ -15,6 +16,20 @@
         "x-scheme-handler/unknown" = "LibreWolf.desktop";
         "application/x-desktop" = "alacritty.desktop";
       };
+    };
+
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      
+      desktop = "${config.home.homeDirectory}/desk";
+      documents = "${config.home.homeDirectory}/docs";
+      download = "${config.home.homeDirectory}/downloads";
+      music = "${config.home.homeDirectory}/music";
+      pictures = "${config.home.homeDirectory}/pics";
+      publicShare = "${config.home.homeDirectory}/pub";
+      templates = "${config.home.homeDirectory}/templates";
+      videos = "${config.home.homeDirectory}/vids";
     };
   };
 }
