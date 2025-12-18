f3205ff50b7263555b2cd99f147fff655898e3ec
Invra <identificationsucks@gmail.com>
2025-12-19 06:24:55 +1100

    refactor mangowc config

--- modules/home/mango/startup-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
-systemctl --user restart xdg-desktop-portal
-systemctl --user restart xdg-desktop-portal-wlr
