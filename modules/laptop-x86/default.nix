6b8ac10f60d4bb94e9afaaa2c19ec63a55dd7b35
Invra <identificationsucks@gmail.com>
2026-01-06 14:53:52 +1100

    chore: format

--- modules/laptop-x86/default.nix
+++ modules/laptop-x86/default.nix
@@ -1,14 +1,14 @@
 { config, ... }: {
   configurations.nixos.laptop-x86.module = {
+    imports = with config.flake.modules.nixos; [
+      base
+      nvidia-gpu
+    ];
     networking = {
       hostId = "0e8e163d";
       hostName = "NixOS";
     };
     facter.reportPath = ./facter.json;
-    imports = with config.flake.modules.nixos; [
-      base
-      nvidia-gpu
-    ];
     hardware.nvidia.open = false;
     system.stateVersion = "24.11";
   };
