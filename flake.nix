e5961b277babe16e4a0c39ce1268d4a0ec419ed9
Invra <identificationsucks@gmail.com>
2026-01-06 19:50:17 +1100

    flake:

--- flake.nix
+++ flake.nix
@@ -21,14 +21,6 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
 
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs = {
-        flake-compat.follows = "dedupe_flake-compat";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -57,32 +49,11 @@
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    refjump-nvim = {
-      flake = false;
-      url = "github:mawkler/refjump.nvim";
-    };
-
-    sink-rotate = {
-      url = "github:mightyiam/sink-rotate";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "dedupe_systems";
-        treefmt-nix.follows = "treefmt-nix";
-      };
-    };
-
-    smart-scrolloff-nvim = {
-      flake = false;
-      url = "github:tonymajestro/smart-scrolloff.nvim";
-    };
-
     statix = {
       url = "github:molybdenumsoftware/statix";
       inputs = {
         flake-parts.follows = "flake-parts";
         nixpkgs.follows = "nixpkgs";
-        systems.follows = "dedupe_systems";
       };
     };
 
@@ -91,15 +62,7 @@
       inputs = {
         flake-parts.follows = "flake-parts";
         nixpkgs.follows = "nixpkgs";
-        nur.follows = "dedupe_nur";
-        systems.follows = "dedupe_systems";
-        tinted-schemes.follows = "tinted-schemes";
       };
-    };
-
-    tinted-schemes = {
-      flake = false;
-      url = "github:tinted-theming/schemes";
     };
 
     treefmt-nix = {
@@ -111,83 +74,14 @@
       url = "github:e-tho/ucodenix";
       inputs.cpu-microcodes.follows = "cpu-microcodes";
     };
-
-    vim-autoread = {
-      flake = false;
-      url = "github:djoshea/vim-autoread/24061f84652d768bfb85d222c88580b3af138dab";
-    };
-
-    zsh-auto-notify = {
-      flake = false;
-      url = "github:MichaelAquilina/zsh-auto-notify";
-    };
   };
 
   # _additional_ `inputs` only for deduplication
-  inputs = {
-    dedupe_flake-compat.url = "github:edolstra/flake-compat";
-
-    dedupe_nur = {
-      url = "github:nix-community/NUR";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-
-    dedupe_systems.url = "github:nix-systems/default";
-  };
+  inputs.dedupe_flake-compat.url = "github:edolstra/flake-compat";
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      text.readme.parts = {
-        disallow-warnings =
-          # markdown
-          ''
-            ## Trying to disallow warnings
-
-            This at the top level of the `flake.nix` file:
-
-            ```nix
-            nixConfig.abort-on-warn = true;
-            ```
-
-            > [!NOTE]
-            > It does not currently catch all warnings Nix can produce, but perhaps only evaluation warnings.
-          '';
-
-        flake-inputs-dedupe-prefix =
-          # markdown
-          ''
-            ## Flake inputs for deduplication are prefixed
-
-            Some explicit flake inputs exist solely for the purpose of deduplication.
-            They are the target of at least one `<input>.inputs.<input>.follows`.
-            But what if in the future all of those targeting `follows` are removed?
-            Ideally, Nix would detect that and warn.
-            Until that feature is available those inputs are prefixed with `dedupe_`
-            and placed in an additional separate `inputs` attribute literal
-            for easy identification.
-
-          '';
-
-        automatic-import =
-          # markdown
-          ''
-            ## Automatic import
-
-            Nix files (they're all flake-parts modules) are automatically imported.
-            Nix files prefixed with an underscore are ignored.
-            No literal path imports are used.
-            This means files can be moved around and nested in directories freely.
-
-            > [!NOTE]
-            > This pattern has been the inspiration of [an auto-imports library, import-tree](https://github.com/vic/import-tree).
-
-          '';
-      };
-
       imports = [ (inputs.import-tree ./modules) ];
 
       _module.args.rootPath = ./.;
