bbf4f47d2de4b928dac675b6468b394715196e07
Invra <identificationsucks@gmail.com>
2026-01-07 16:44:04 +1100

    forgot

--- modules/vdam-x86/default.nix
+++ modules/vdam-x86/default.nix
@@ -1,9 +1,6 @@
 { config, ... }: {
   configurations.nixos.vdam-x86.module = {
-    imports = with config.flake.modules.nixos; [
-      base
-      nvidia-gpu
-    ];
+    imports = with config.flake.modules.nixos; [ base ];
     networking = {
       hostId = "0e8e163d";
       hostName = "NixOS";
