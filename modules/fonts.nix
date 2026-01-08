4a3383abe50ea01885bf81d94f8974ec0bdeaf1a
Invra <identificationsucks@gmail.com>
2026-01-09 05:05:33 +1100

    add: fonts

---
+++ modules/fonts.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.nixos.base = { pkgs, ... }: {
+    fonts = {
+      fontconfig.defaultFonts.monospace = [ "JetBrainsMono" ];
+
+      packages = with pkgs; [
+        nerd-fonts.jetbrains-mono
+        font-awesome
+        liberation_ttf
+        noto-fonts
+        noto-fonts-color-emoji
+      ];
+    };
+  };
+}
