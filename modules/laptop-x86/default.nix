5578139931086348b79ed77c289ef4484bce58f4
Invra <identificationsucks@gmail.com>
2026-01-08 17:21:37 +1100

    fix: device not found issue

--- modules/laptop-x86/default.nix
+++ modules/laptop-x86/default.nix
@@ -10,9 +10,21 @@
     };
     facter.reportPath = ./facter.json;
     hardware.nvidia.open = false;
+
+    boot = {
+      initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "ahci" "nvme" ];
+      kernelModules = [ "kvm-intel" ];
+    };
+    
     fileSystems = {
-      "/".device = "/dev/nvme0n1p3";
-      "/home".device = "/dev/nvme0n1p4";
+      "/" = {
+        device = "/dev/disk/by-uuid/0522bf8e-6244-4430-a3c6-c5898c9b6b7b";
+        fsType = "ext4";
+      };
+      "/home" = {
+        device = "/dev/disk/by-uuid/068428c3-c663-4955-849e-b595841e273f";
+        fsType = "ext4";
+      };
     };
     system.stateVersion = "24.11";
   };
