0dae3086f575a23d40a6ea204792d591dadc21ee
Invra <identificationsucks@gmail.com>
2026-01-09 04:43:03 +1100

    chore: clean up more stuff

---
+++ modules/nix.nix
@@ -0,0 +1,30 @@
+{ lib, ... }:
+let
+  polyModule = { pkgs, ... }: {
+    nix.package = pkgs.nixVersions
+      |> lib.attrNames
+      |> lib.filter (lib.hasPrefix "nix_")
+      |> lib.naturalSort
+      |> lib.last
+      |> lib.flip lib.getAttr pkgs.nixVersions
+      |> lib.mkDefault;
+
+    nix.settings = {
+      keep-outputs = true;
+      experimental-features = [
+        "flakes"
+        "nix-command"
+        "recursive-nix"
+      ];
+      extra-system-features = [
+        "recursive-nix"
+      ];
+    };
+  };
+in
+{
+  flake.modules = {
+    nixos.base = polyModule;
+    homeManager.base = polyModule;
+  };
+}
