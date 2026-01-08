029333603360b95d6ca3609bd1468e7fc69e3f38
Invra <identificationsucks@gmail.com>
2026-01-08 18:22:24 +1100

    add: helix

--- flake.nix
+++ flake.nix
@@ -9,35 +9,27 @@
       flake = false;
       url = "github:platomav/CPUMicrocodes";
     };
-
     dev-nix.url = "gitlab:invra/nix-dev";
-
+    discord-rpc-lsp.url = "gitlab:invra/discord-rpc-lsp";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     import-tree.url = "github:vic/import-tree";
-
     mango = {
       url = "github:DreamMaoMao/mango";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixcord = {
       url = "github:FlameFlag/nixcord";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     stylix = {
       url = "github:danth/stylix";
       inputs = {
@@ -45,12 +37,10 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
-
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     ucodenix = {
       url = "github:e-tho/ucodenix";
       inputs.cpu-microcodes.follows = "cpu-microcodes";
