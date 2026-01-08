d9fd88c33c835fb0bb6fa8a712176116e67efd91
Invra <identificationsucks@gmail.com>
2026-01-09 04:57:00 +1100

    chore: cleanup fs

---
+++ modules/meta.nix
@@ -0,0 +1,37 @@
+{ config, inputs, lib, ... }:
+{
+  imports = [ inputs.flake-parts.flakeModules.modules ];
+
+  
+  options.flake.meta = lib.mkOption {
+    type = lib.types.anything;
+  };
+  config = {
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+
+    flake = {
+      meta.owner = {
+        email = "identificationsucks@gmail.com";
+        name = "Invra";
+        username = "invra";
+        matrix = "@invranet:matrix.org";
+      };
+
+      modules = {
+        nixos.base = {
+          users.users.${config.flake.meta.owner.username} = {
+            isNormalUser = true;
+            initialPassword = "";
+            extraGroups = [ "input" ];
+          };
+
+          nix.settings.trusted-users = [ config.flake.meta.owner.username ];
+        };
+      };
+    };
+  };
+}
